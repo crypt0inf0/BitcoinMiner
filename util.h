3d1e14b5da3ecc74cade06008fe024d4707ffd17
crypt0inf0 <84570125+crypt0inf0@users.noreply.github.com>
2024-05-17 11:13:32 +0530

    Update util.h
    Added decimal to hex and big endian to little endian

--- util.h
+++ util.h
@@ -1,5 +1,7 @@
 #include <stdint.h>
 
+void decimal_to_hex(uint32_t decimal, char* hex_str, size_t hex_str_size);
+void convert_to_little_endian(char* hex_str);
 void print_bytes(const unsigned char *data, size_t dataLen, bool format = true);
 void print_bytes_reversed(const unsigned char *data, size_t dataLen, bool format = true);
 uint32_t Reverse32(uint32_t value);
